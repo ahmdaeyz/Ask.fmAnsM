453671bdc754f7165388989a62395dc0cb8518c9
Ahmed Aboelyazeed <ahmedalarabe5@gmail.com>
2018-09-23 17:55:57 +0200

    creates a directory and downloads to it

--- answerMedia.go
+++ answerMedia.go
@@ -68,7 +68,7 @@ func (media media) downloadMedia() error {
 	if check, _ := exists("./storage/pictures/Ask.fm_Media"); !check {
 		os.Mkdir("./storage/pictures/Ask.fm_Media", 0777)
 	}
-	file, _ := os.Create("./storage/pictures/Ask.fm_Media" + media.URL.String()[strings.LastIndex(media.URL.String(), "/")+1:])
+	file, _ := os.Create("./storage/pictures/Ask.fm_Media/" + media.URL.String()[strings.LastIndex(media.URL.String(), "/")+1:])
 	req, err := client.Get(media.URL.String())
 	if err != nil {
 		log.Fatal(err)
